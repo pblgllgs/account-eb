de25cda2ff9b926b1d1567650e143dfb846f8df9
pbl.gllgs <pbl.gllgs@gmail.com>
2023-04-14 13:00:17 -0400

    config server

--- src/main/java/com/pblgllgs/account/controller/AccountController.java
+++ src/main/java/com/pblgllgs/account/controller/AccountController.java
@@ -1,18 +1,28 @@
 package com.pblgllgs.account.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.pblgllgs.account.config.AccountsServiceConfig;
 import com.pblgllgs.account.model.Account;
 import com.pblgllgs.account.model.Customer;
+import com.pblgllgs.account.model.Properties;
 import com.pblgllgs.account.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class AccountController {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    AccountsServiceConfig accountsConfig;
 
     @PostMapping("/myAccount")
     public Account getAccountDetails(@RequestBody Customer customer){
@@ -22,5 +32,14 @@ public class AccountController {
         }else{
             return null;
         }
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
