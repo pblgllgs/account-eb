3982f07fcecb0b02e69be78461d403654051e625
pbl.gllgs <pbl.gllgs@gmail.com>
2023-04-18 10:53:41 -0400

    update service registry eureka server

--- src/main/java/com/pblgllgs/account/controller/AccountController.java
+++ src/main/java/com/pblgllgs/account/controller/AccountController.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.pblgllgs.account.config.AccountsServiceConfig;
-import com.pblgllgs.account.model.Account;
-import com.pblgllgs.account.model.Customer;
-import com.pblgllgs.account.model.Properties;
+import com.pblgllgs.account.model.*;
 import com.pblgllgs.account.repository.AccountRepository;
+import com.pblgllgs.account.service.client.CardsFeignClient;
+import com.pblgllgs.account.service.client.LoansFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -23,6 +25,12 @@ public class AccountController {
 
     @Autowired
     AccountsServiceConfig accountsConfig;
+
+    @Autowired
+    LoansFeignClient loansFeignClient;
+
+    @Autowired
+    CardsFeignClient cardsFeignClient;
 
     @PostMapping("/myAccount")
     public Account getAccountDetails(@RequestBody Customer customer){
@@ -41,5 +49,20 @@ public class AccountController {
                 accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
         String jsonStr = ow.writeValueAsString(properties);
         return jsonStr;
+    }
+
+    @PostMapping("/myCustomerDetails")
+    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+        Account accounts = accountRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccount(accounts);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+
+        return customerDetails;
+
     }
 }
---
+++ src/main/java/com/pblgllgs/account/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.pblgllgs.account.service.client;
+
+import com.pblgllgs.account.model.Cards;
+import com.pblgllgs.account.model.Customer;
+import com.pblgllgs.account.model.Loans;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+    @RequestMapping(method = RequestMethod.POST, value = "myLoans" , consumes = "application/json")
+    List<Loans> getLoansDetails(@RequestBody Customer costumer);
+}
